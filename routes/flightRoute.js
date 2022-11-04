435de3e89593060d14eaf29f0442e28e27bd57ec
wiskky <adeniyi.wisdom@gmail.com>
2022-11-04 21:43:58 +0100

    first commit

---
+++ routes/flightRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+const controller = require('../controllers/flightController');
+
+router.get('/', controller.example)
+router.get('/', controller.example)
+router.get('/', controller.example)
+
+module.exports = router;
+
